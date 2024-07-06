d089b1212d7e13402f5b10a778991a60bf1b894a
dtg <3364659916@qq.com>
2024-07-06 11:13:23 +0800

    在2while.c中修复了99乘法表打印功能，并添加了新的实现方式。

--- C语言/2while.c
+++ C语言/2while.c
@@ -1,5 +1,6 @@
 //打印99乘法表
 #include <stdio.h>
+#if 0
 int main()
 {
     int i=0,j=0;
@@ -15,3 +16,15 @@ int main()
         i++;
     }
 }
+#endif
+#if 1
+int main()
+{
+    for(int i=1;i<10;i++)
+    {
+        for(int j=1;j<=i;j++)
+            printf("%d*%d=%d\t",j,i,i*j);
+        printf("\n");
+    }
+}
+#endif
